e5ffe935a8917a89368582d05d78f3e2096eaae3
DibbenduBanikPurna <purna.banik164487@gmail.com>
2022-11-15 13:34:23 +0600

    done

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
+// external import 
 const express=require('express')
 const cors=require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId
 const fileUpload = require('express-fileupload')
 
+//express cal by app
 const app=express()
-app.use(express())
+app.use(express())//express defined
 app.use(express.json())
 app.use(cors())
 app.use(fileUpload())
 
-//AIzaSyAACG3mLEXEUEDqPCvh6ZFteaLQDnH0YbI
+//mongodb uri
 const uri = "mongodb+srv://purna:2470purna@cluster0.z2een.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
         await client.connect();
+        //database name
         const database = client.db('Society-of-ict');
-         const teacherCollcetion = database.collection('teachers');
+        //database collection name 
+        const teacherCollcetion = database.collection('teachers');
      const jobHolderCollcetion = database.collection('jobholder');
         const studentCollcetion = database.collection('student');
 
@@ -26,55 +30,55 @@ async function run() {
 
         
 
-        //post new member
+        //post new student 
         app.post('/addmember', async(req,res)=>{
             const user = req.body;
             console.log(req.body)
              const result = await studentCollcetion.insertOne(user)
-             console.log(result)
-             res.json("result")
+            // console.log(result)
+             res.json("Student Data added successfully");
         })
         
-        //post data data
+        //post job holder all data
 
         app.post('/jobs', async (req, res) => {
             const user = req.body;
             console.log(req.body)
              const result = await jobHolderCollcetion.insertMany(user)
-             console.log(result)
+             //console.log(result)
              res.json(result)
         })
         
         //post user data
 
-        app.post('/studentdata', async (req, res) => {
-            console.log(req.body);
-            const name = req.body.name;
-            const email = req.body.email;
-            const image = req.files.image;
-            const id=req.body.id;
-            const session=req.body.session;
-            const blood=req.body.blood;
-            const skill=req.body.skill;
-                //console.log(req.body);
-            const picData = image.data;
-            const encodedPic = picData.toString('base64')
-            const imageBuffer = Buffer.from(encodedPic, 'base64')
-            const doctor = {
-                name,
-                email,
-                image: imageBuffer,
-                id,
-                session,
-                blood,
-                skill
-            }
-            const result = await studentCollcetion.insertOne(doctor);
+        // app.post('/studentdata', async (req, res) => {
+        //     console.log(req.body);
+        //     const name = req.body.name;
+        //     const email = req.body.email;
+        //     const image = req.files.image;
+        //     const id=req.body.id;
+        //     const session=req.body.session;
+        //     const blood=req.body.blood;
+        //     const skill=req.body.skill;
+        //         //console.log(req.body);
+        //     const picData = image.data;
+        //     const encodedPic = picData.toString('base64')
+        //     const imageBuffer = Buffer.from(encodedPic, 'base64')
+        //     const doctor = {
+        //         name,
+        //         email,
+        //         image: imageBuffer,
+        //         id,
+        //         session,
+        //         blood,
+        //         skill
+        //     }
+        //     const result = await studentCollcetion.insertOne(doctor);
 
-           console.log(result)
-            res.json("result")
+        //    console.log(result)
+        //     res.json("result")
 
-        })
+        // })
 
         // get student data
         app.get('/members',async(req,res)=>{
@@ -115,7 +119,13 @@ async function run() {
             res.send(result)
         })
 
-        //single student data get
+        app.get('/student/:email',async(req,res)=>{
+            console.log(req.params.email)
+            const result = await studentCollcetion.findOne({email:req.params.email })
+            res.send(result)
+            //console.log(result)
+        })
+        //single  student search data get by id
         app.get('/singlestudent/:id', async(req,res)=>{
             const result = await studentCollcetion.findOne({_id: ObjectId(req.params.id) })
                      res.send(result)
@@ -157,7 +167,7 @@ run().catch(console.dir);
 
 
 app.get('/',(req,res)=>{
-    res.send("Welcome to dept seminer library");
+    res.send("WELCOME TO  SOCIETY OF ICT");
 })
 
 
